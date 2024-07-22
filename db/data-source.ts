def86f7efa14d9521338abe9f8e24a087947ca19
Rayane-Boucheraine <rayaneha08@gmail.com>
2024-07-22 10:47:30 +0100

    db config

---
+++ db/data-source.ts
@@ -0,0 +1,20 @@
+import { DataSource, DataSourceOptions } from "typeorm";
+import { config } from "dotenv";
+
+config()
+export const dataSourceOptions: DataSourceOptions = {
+  type: 'postgres',
+  database: process.env.DB_DATABASE,
+  host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT),
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  entities: [],
+  migrations: [],
+  logging: false,
+  synchronize: false,
+};
+
+const dataSource = new DataSource(dataSourceOptions)
+
+export default dataSource
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { dataSourceOptions } from 'db/data-source';
 
 @Module({
-  imports: [],
+  imports: [TypeOrmModule.forRoot(dataSourceOptions)],
   controllers: [AppController],
   providers: [AppService],
 })
